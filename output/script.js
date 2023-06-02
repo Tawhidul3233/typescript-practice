76a1b47032e130bc42789302813eab49da9c30f9
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-06-02 15:04:03 +0600

    first commit

---
+++ output/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const myFunk = () => {
+    console.log('Hello TypeScript');
+};
+console.log(myFunk());
+console.log('Hello world');
+console.log('Hello world');
---
+++ src/script.ts
@@ -0,0 +1,5 @@
+const myFunk = () => {
+  console.log('Hello TypeScript')
+}
+console.log(myFunk())
+console.log('Hello world')
