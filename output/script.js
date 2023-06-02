be1ca716c8eb4573bc8af2fd5fd1ec9047cb76f9
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-06-02 20:30:21 +0600

    data type practic

--- output/script.js
+++ output/script.js
@@ -1,7 +1,31 @@
 "use strict";
-const myFunk = () => {
-    console.log('Hello TypeScript');
-};
-console.log(myFunk());
-console.log('Hello world');
-console.log('Hello world');
+let userName = 'Akash';
+// userName can't assing number
+// userName = 55  
+userName = 'Babul';
+// console.log(userName)
+let rollNumber = 5;
+// rollNumber = '5' con't assing as a string value
+rollNumber = 1;
+// console.log(rollNumber)
+let data;
+// data = 'all data' can't assing string value must assing object and must have name string and id number,
+data = { name: 'akash', id: 555 };
+// console.log(data);
+let dataArray;
+// dataArray = 5 can't assing with out array
+dataArray = ['e', 'r'];
+// console.log(dataArray);
+let arrayOfObject;
+arrayOfObject = [{ name: 'akash', id: 40 }];
+// console.log(arrayOfObject)
+let a;
+a = 'akash';
+a = 66;
+a = { name: 'akash', id: 66 };
+a = ['c', 't'];
+a = undefined;
+a = null,
+    a = true,
+;
+// if type any we can assign any type of value
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,35 @@
-const myFunk = () => {
-  console.log('Hello TypeScript')
-}
-console.log(myFunk())
-console.log('Hello world')
+let userName : string = 'Akash'
+// userName can't assing number
+// userName = 55  
+userName = 'Babul'
+// console.log(userName)
+
+let rollNumber: number = 5;
+// rollNumber = '5' con't assing as a string value
+rollNumber= 1;
+// console.log(rollNumber)
+
+let data : {name: string, id: number} 
+// data = 'all data' can't assing string value must assing object and must have name string and id number,
+data = {name: 'akash', id: 555};
+// console.log(data);
+
+let dataArray: string[];
+// dataArray = 5 can't assing with out array
+dataArray = ['e','r'];
+// console.log(dataArray);
+
+let arrayOfObject : object[];
+arrayOfObject = [{name: 'akash', id: 40}];
+// console.log(arrayOfObject)
+
+let a: any;
+a= 'akash';
+a= 66;
+a= {name: 'akash', id: 66};
+a= ['c', 't'];
+a= undefined;
+a = null,
+a = true,
+// if type any we can assign any type of value
+
